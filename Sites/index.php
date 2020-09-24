cdf81695d495457aeb443d7a10430260072eb78f
Agustin <aberguecio1@uc.cl>
2020-09-24 12:12:09 -0300

    index

--- Sites/index.php
+++ Sites/index.php
@@ -1,6 +1,6 @@
 <?php
   $user = 'grupo97';
-  $password = 'grupo97';
+  $password = 'langosta';
   $databaseName = 'grupo97e2';
   $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
 
