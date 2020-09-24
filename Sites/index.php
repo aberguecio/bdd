c758fe53cc0b7d326c58e33a0c2570ad4c828c29
Agustin <aberguecio1@uc.cl>
2020-09-24 12:06:07 -0300

    index

---
+++ Sites/index.php
@@ -0,0 +1,25 @@
+<?php
+  $user = 'grupo97';
+  $password = 'grupo97';
+  $databaseName = 'grupo97e2';
+  $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+
+  $query_string = "SELECT * FROM tabla;";
+  $query = $db -> prepare($query_string);
+  $query -> execute();
+  $result = $query -> fetchAll();
+?>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Altura</th>
+  </tr>
+  
+  <?php
+    foreach ($result as $r) {
+      echo "<tr><td>$r[0]</td><</tr>";
+    }
+  ?>
+</table>
