6cb35271f952aa00320ae9dab244100a0da85f59
Agustin <aberguecio1@uc.cl>
2020-09-26 13:30:28 -0300

    test

--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -2,15 +2,13 @@
   require("../config/conexion.php");
 
   $nombre_puerto = ($_GET["nombre_puerto"]);
-  echo $nombre_puerto;
+  #echo $nombre_puerto;
 
   $query_string = "SELECT rut_jefe FROM Instalaciones WHERE nombre_puerto = '%$nombre_puerto%'"; #'%$nombre_puerto%'
   $query = $db -> prepare($query_string);
   $query -> execute();
   $result = $query -> fetchAll();
 ?>
-
-
 <table>
   <?php
     foreach ($result as $r) {
