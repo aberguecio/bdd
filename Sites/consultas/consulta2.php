54373c8cadda7010a7eed78a985caa6ede4c7033
Agustin <aberguecio1@uc.cl>
2020-09-26 13:12:47 -0300

    test

--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -1,7 +1,7 @@
 <?php
   require("../config/conexion.php");
 
-  $nombre = $_POST["nombre_puerto"];
+  $nombre_puerto = $_POST["nombre_puerto"];
 
   $query_string = "SELECT rut_jefe FROM Instalaciones WHERE nombre_puerto ='%$nombre_puerto%';";
   $query = $db -> prepare($query_string);
