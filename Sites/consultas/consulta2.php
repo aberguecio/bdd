f6aa65011367278cc29c80caae921686a9aca1cf
Agustin <aberguecio1@uc.cl>
2020-09-26 13:19:33 -0300

    test

--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -1,9 +1,9 @@
 <?php
   require("../config/conexion.php");
 
-  $nombre_puerto = $_POST["nombre_puerto"];
+  $nombre_puerto = ($_GET["nombre_puerto"]);
 
-  $query_string = "SELECT rut_jefe FROM Instalaciones WHERE nombre_puerto = 'Mejillones'"; #'%$nombre_puerto%'
+  $query_string = "SELECT rut_jefe FROM Instalaciones WHERE nombre_puerto = '$nombre_puerto'"; #'%$nombre_puerto%'
   $query = $db -> prepare($query_string);
   $query -> execute();
   $result = $query -> fetchAll();
