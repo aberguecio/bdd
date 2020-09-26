8ae599407a0b99fd133587ca2ce35dac02cbe794
Agustin <aberguecio1@uc.cl>
2020-09-26 13:10:14 -0300

    test

--- Sites/index.php
+++ Sites/index.php
@@ -4,35 +4,35 @@
 ?>
 
 
-<form align="center" action="consulta1.php" method="get">
+<form align="center" action="consultas/consulta1.php" method="get">
   <p>Consulta 1</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
 
-<form align="center" action="consulta2.php" method="post">
+<form align="center" action="consultas/consulta2.php" method="post">
     Nombre:
     <input type="text" name="nombre_puerto">
     <input type="submit" value="Buscar">
 </form>
 
 
-<form action="consulta2.php" method="get">
+<form action="consultas/consulta2.php" method="get">
   <p>Consulta 2</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
-<form action="consulta3.php" method="get">
+<form action="consultas/consulta3.php" method="get">
   <p>Consulta 3</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
-<form action="consulta4.php" method="get">
+<form action="consultas/consulta4.php" method="get">
   <p>Consulta 4</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
-<form action="consulta5.php" method="get">
+<form action="consultas/consulta5.php" method="get">
   <p>Consulta 5</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
-<form action="consulta6.php" method="get">
+<form action="consultas/consulta6.php" method="get">
   <p>Consulta 6</p>
   <p><input type="submit" name="respuesta" value="ir"></p>
 </form>
