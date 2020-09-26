9961b55b582e0a4d1d533204edbcc07efd44902c
Agustin <aberguecio1@uc.cl>
2020-09-26 14:18:17 -0300

    test

--- Sites/consultas/consulta4.php
+++ Sites/consultas/consulta4.php
@@ -5,7 +5,7 @@
   $barco = $_POST["barco"];
   echo "$barco , $puerto";
 
-  $query_string = "SELECT COUNT(Puertos.ciudad_puerto) FROM Puertos, (SELECT nombre_puerto FROM Instalaciones, (SELECT id_instalacion FROM Barcos, Permisos WHERE Barcos.patente_barco = Permisos.patente_barco AND Barcos.patente_barco = (SELECT patente_barco FROM Barcos WHERE nombre_barco LIKE '$%barco%')) as foo WHERE Instalaciones.id_instalacion = foo.id_instalacion) AS fua WHERE Puertos.nombre_puerto = fua.nombre_puerto AND Puertos.ciudad_puerto LIKE '%$puerto%';";
+  $query_string = "SELECT COUNT(Puertos.ciudad_puerto) FROM Puertos, (SELECT nombre_puerto FROM Instalaciones, (SELECT id_instalacion FROM Barcos, Permisos WHERE Barcos.patente_barco = Permisos.patente_barco AND Barcos.patente_barco = (SELECT patente_barco FROM Barcos WHERE nombre_barco LIKE '%$barco%')) as foo WHERE Instalaciones.id_instalacion = foo.id_instalacion) AS fua WHERE Puertos.nombre_puerto = fua.nombre_puerto AND Puertos.ciudad_puerto LIKE '%$puerto%';";
   $query = $db -> prepare($query_string);
   $query -> execute();
   $result = $query -> fetchAll();
