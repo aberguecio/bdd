0f7549a1e8aeab2f5c55b555ee78d5e8a398dead
Agustin <aberguecio1@uc.cl>
2020-09-26 21:12:36 -0300

    5y6

--- Sites/consultas/consulta6.php
+++ Sites/consultas/consulta6.php
@@ -3,6 +3,7 @@
 
   $mes = $_POST["mes"];
   $mes2= $mes + 1;
+  echo "$mes y $mes2"
 
   $query_string = "SELECT nombre_puerto FROM (SELECT nombre_puerto, COUNT(nombre_puerto) FROM Instalaciones, (SELECT id_instalacion FROM Permisos WHERE fecha_atraque>'2020-$mes-01 00:00:00' AND fecha_atraque<'2020-$mes2-01 00:00:00') as foo WHERE Instalaciones.id_instalacion = foo.id_instalacion GROUP BY nombre_puerto) AS fua WHERE fua.count = (SELECT MAX(count) FROM (SELECT nombre_puerto, COUNT(nombre_puerto) FROM Instalaciones, (SELECT id_instalacion FROM Permisos WHERE fecha_atraque>'2020-08-01 00:00:00' AND fecha_atraque<'2020-09-01 00:00:00') as foo WHERE Instalaciones.id_instalacion = foo.id_instalacion GROUP BY nombre_puerto) AS foo);";
   $query = $db -> prepare($query_string);
