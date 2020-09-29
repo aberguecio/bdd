3fd14363e11a1bddb722ba90446c49d0e01299c0
Agustin <aberguecio1@uc.cl>
2020-09-29 16:39:23 -0300

    arreglo

--- Sites/index.php
+++ Sites/index.php
@@ -1,106 +1,69 @@
+<?php
+  require("config/conexion.php");
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Regna Bootstrap Template</title>
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <meta content="" name="keywords">
-  <meta content="" name="description">
+<form align="center" action="consultas/consulta1.php" method="post">
+  Muestre todos los puertos junto la ciudad a la que son asignados.
+  <input type="submit" name="respuesta" value="Buscar">
+</form>
 
-  <!-- Favicons -->
-  <link href="img/favicon.png" rel="icon">
-  <link href="img/apple-touch-icon.png" rel="apple-touch-icon">
-
-
-  <!-- Bootstrap CSS File -->
-  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<form align="center" action="consultas/consulta2.php" method="post">
+  Muestre todos los jefes de las instalaciones del puerto con nombre:
+    <input type="text" name="nombre_puerto">
+    <input type="submit" value="Buscar">
+</form>
 
 
-  <!-- Main Stylesheet File -->
-  <link href="css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-    Theme Name: Regna
-    Theme URL: https://bootstrapmade.com/regna-bootstrap-onepage-template/
-    Author: BootstrapMade.com
-    License: https://bootstrapmade.com/license/
-  ======================================================= -->
-</head>
-
-
-<body>
-
-  <!--==========================
-  Header
-  ============================-->
-  <header id="header">
-    <div class="container">
-
-      <div id="logo" class="pull-left">
-        <h1><a href="#hero">BDD</a></h1>
-      </div>
-
-      
-    </div>
-  </header><!-- #header -->
-
-  <!--==========================
-    Hero Section
-  ============================-->
-  <section id="hero">
-    <div class="hero-container">
-      <h1>Welcome to Regna</h1>
-      <h2>We are team of talanted designers making websites with Bootstrap</h2>
-      <a href="#about" action="index2.php" class="btn-get-started" type="submit" value="Buscar">Get Started</a> 
-      <input type="submit" value="Buscar">
-    </div>
-  </section><!-- #hero -->
-
-  <main id="main">
-
-    
-
-    <!--==========================
-      Team Section
-    ============================-->
-    <section id="team">
-      <div class="container wow fadeInUp">
-        <div class="section-header">
-          <h3 class="section-title">Team</h3>
-          <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
-        </div>
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="img/team-1.jpg" alt=""></div>
-              <h4>Walter White</h4>
-              <span>Chief Executive Officer</span>
-
-              </div>
-            </div>
-          </div>
-
-          
-
-          <div class="col-lg-3 col-md-6">
-            <div class="member">
-              <div class="pic"><img src="img/team-4.jpg" alt=""></div>
-              <h4>Amanda Jepson</h4>
-              <span>Accountant</span>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </section><!-- #team -->
-
-
-</body>
-</html>
+  <?php
+  $result = $db -> prepare("SELECT DISTINCT tipo_instalacion FROM Instalaciones;");
+  $result -> execute();
+  $dataCollected = $result -> fetchAll();
+  ?>
+  <form align="center" action="consultas/consulta3.php" method="post">
+  Muestre todos los puertos que tienen al menos un:
+    <select name="tipo">
+      <?php
+      foreach ($dataCollected as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    <input type="submit" value="Buscar">
+  </form>
 
 
 
+  <?php
+  $result = $db -> prepare("SELECT DISTINCT ciudad_puerto FROM Puertos;");
+  $result -> execute();
+  $datapuertos = $result -> fetchAll();
+  ?>
+  <form align="center" action="consultas/consulta4.php" method="post">
+  Muestre todas las veces que ha atracado en:
+    <select name="puerto">
+      <?php
+      foreach ($datapuertos as $d) {
+        echo "<option value=$d[0]>$d[0]</option>";
+      }
+      ?>
+    </select>
+    el barco:
+    <input type="text" name="barco">
+    <input type="submit" value="Buscar">
+  </form>
+
+
+<form align="center" action="consultas/consulta5.php" method="post">
+ Muestre la edad promedio de los trabajadores de cada puerto 
+  <input type="submit" name="respuesta" value="Buscar">
+</form>
+
+<form align="center" action="consultas/consulta6.php" method="post">
+Muestre el puerto que ha recibido más barcos en 
+  Mes:
+  <input type="number" name="mes">
+  Año:
+  <input type="number" name="año">
+
+  <input type="submit" name="respuesta" value="buscar">
+</form>
