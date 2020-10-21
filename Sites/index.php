456dfc07fed34eb5516a8f6ee36a80e31a014f12
Agustin <aberguecio1@uc.cl>
2020-10-21 11:38:53 -0300

    test

--- Sites/index.php
+++ Sites/index.php
@@ -2,31 +2,12 @@
   require("config/conexion.php");
 ?>
 
-<form align="center" action="index.php" method="post">
-  consultas
+<form align="center" action="login.php" method="post">
+  login
   <input type="submit" name="respuesta" value="Buscar">
 </form>
 
-<form align="center" action="consultas/consulta2.php" method="post">
-  Muestre todos los jefes de las instalaciones del puerto con nombre:
-    <input type="text" name="nombre_puerto">
-    <input type="submit" value="Buscar">
+<form align="center" action="registrarme.php" method="post">
+  consultas
+  <input type="submit" name="respuesta" value="Buscar">
 </form>
-
-
-  <?php
-  $result = $db -> prepare("SELECT DISTINCT tipo_instalacion FROM Instalaciones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-  <form align="center" action="consultas/consulta3.php" method="post">
-  Muestre todos los puertos que tienen al menos un:
-    <select name="tipo">
-      <?php
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    <input type="submit" value="Buscar">
-  </form>
---
+++ Sites/registrarme.php
@@ -0,0 +1,12 @@
+<?php
+  require("config/conexion.php");
+?>
+
+<form align="center" action="consultas.php" method="post">
+  Muestre todos los jefes de las instalaciones del puerto con nombre:
+    <input type="text" name="pasaporte">
+    <input type="text" name="contraseña">
+    <input type="text" name="edad">
+    <input type="text" name="bla">
+    <input type="submit" value="ingresar">
+</form>
