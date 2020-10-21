fed5ab4bec4779091b2f4917d3336e0ff1610ab3
Agustin <aberguecio1@uc.cl>
2020-10-21 11:13:49 -0300

    test

--- Sites/index.php
+++ Sites/index.php
@@ -2,8 +2,8 @@
   require("config/conexion.php");
 ?>
 
-<form align="center" action="consultas/consulta1.php" method="post">
-  Muestre todos los puertos junto la ciudad a la que son asignados.
+<form align="center" action="index.php" method="post">
+  consultas
   <input type="submit" name="respuesta" value="Buscar">
 </form>
 
@@ -30,40 +30,3 @@
     </select>
     <input type="submit" value="Buscar">
   </form>
-
-
-
-  <?php
-  $result = $db -> prepare("SELECT DISTINCT ciudad_puerto FROM Puertos;");
-  $result -> execute();
-  $datapuertos = $result -> fetchAll();
-  ?>
-  <form align="center" action="consultas/consulta4.php" method="post">
-  Muestre todas las veces que ha atracado en:
-    <select name="puerto">
-      <?php
-      foreach ($datapuertos as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
-      }
-      ?>
-    </select>
-    el barco:
-    <input type="text" name="barco">
-    <input type="submit" value="Buscar">
-  </form>
-
-
-<form align="center" action="consultas/consulta5.php" method="post">
- Muestre la edad promedio de los trabajadores de cada puerto 
-  <input type="submit" name="respuesta" value="Buscar">
-</form>
-
-<form align="center" action="consultas/consulta6.php" method="post">
-Muestre el puerto que ha recibido más barcos en 
-  Mes:
-  <input type="number" name="mes">
-  Año:
-  <input type="number" name="año">
-
-  <input type="submit" name="respuesta" value="buscar">
-</form>
