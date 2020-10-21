d2712c5fdd9f0c4cca8c839d939ef6973589ae47
Agustin <aberguecio1@uc.cl>
2020-10-21 14:18:29 -0300

    test

--- Sites/perfil/verificacion_registro.php
+++ Sites/perfil/verificacion_registro.php
@@ -14,24 +14,22 @@
   $query = $db -> prepare($query_string);
   $query -> execute();
   $result = $query -> fetchAll();
-  // $ultimo = $result;
+
 
   foreach ($result as $r)  {
     $ultimo = $r;
-    echo "<tr><td>$r[0]</td><td>$r[1]</td></tr>";
     if ($numero_pasaporte = $r){
-      $ultimo = $r;
       $confirmador = FALSE;
     }
   }
-  // if ($confirmador){
-  //   $query_string = "INSERT INTO Usuarios VALUES ('$numero_pasaporte','$nombre', '$edad','$sexo','$nacionalidad','$contraseña')";
-  //   $query = $db -> prepare($query_string);
-  //   $query -> execute();
-  //   $result = $query -> fetchAll();
-  //   echo "listo, $confirmador, $numero_pasaporte = $ultimo ";
-  // } else {
-  //   echo "ya existe, $confirmador, $numero_pasaporte = $ultimo";
-  // }
-  // ?>
+  if ($confirmador){
+    $query_string = "INSERT INTO Usuarios VALUES ('$numero_pasaporte','$nombre', '$edad','$sexo','$nacionalidad','$contraseña')";
+    $query = $db -> prepare($query_string);
+    $query -> execute();
+    $result = $query -> fetchAll();
+    echo "listo, $confirmador, $numero_pasaporte = $ultimo ";
+  } else {
+    echo "ya existe, $confirmador, $numero_pasaporte = $ultimo";
+  }
+  ?>
 
